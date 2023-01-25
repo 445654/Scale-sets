9cccd1697f54900cb87086f7eaf84cf1910f89f2
445654 <christophe.carmagnac@gmail.com>
2023-01-25 09:02:45 +0100

    modify merge function

--- main.cpp
+++ main.cpp
@@ -4,25 +4,26 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include <set>
 
 using namespace std;
 using namespace cv;
 
+using Pixel = pair<int, int>;
+
 struct Region
 {
-    int id;
-    vector<pair<int, int>> pixels;
-    vector<int> adjacentRegions;
-
+    int id{0};
+    set<Pixel> pixels;
+    set<int> adjacentRegions;
+    int perimeter{0};
 };
-
-vector<Region> REGIONS;
 
 double mean(const Mat &image, const Region &r)
 {
     double sum;
 
-    for (pair<int, int> pixel : r.pixels)
+    for (Pixel pixel : r.pixels)
         sum += image.at<float>(pixel.first, pixel.second);
 
     return sum / r.pixels.size();
@@ -49,7 +50,7 @@ double variance(Mat image, Region r)
     {
         double sum = 0.;
         double meanChannel = mean(channels[i], r);
-        for (pair<int, int> pixel : r.pixels)
+        for (Pixel pixel : r.pixels)
             sum += pow(channels[i].at<float>(pixel.first, pixel.second) - meanChannel, 2);
         variances[i] = sum / r.pixels.size();
     }
@@ -62,25 +63,52 @@ double variance(Mat image, Region r)
     return varianceSum / 3;
 }
 
-vector<pair<int,int>> findBorder(Region r)
+Pixel* getNeighbors(Pixel pixel)
 {
-    vector<pair<int,int>> border;
-    for (pair<int,int> pixel : r.pixels)
-    {
-        pair<int,int> neighbors[4];
-        neighbors[0] = pair<int,int>(pixel.first - 1, pixel.second);
-        neighbors[1] = pair<int,int>(pixel.first, pixel.second + 1);
-        neighbors[2] = pair<int,int>(pixel.first + 1, pixel.second);
-        neighbors[3] = pair<int,int>(pixel.first, pixel.second - 1);
+    Pixel neighbors[4];
+    neighbors[0] = Pixel(pixel.first - 1, pixel.second);
+    neighbors[1] = Pixel(pixel.first, pixel.second + 1);
+    neighbors[2] = Pixel(pixel.first + 1, pixel.second);
+    neighbors[3] = Pixel(pixel.first, pixel.second - 1);
+    return neighbors;
+}
 
-        for (pair<int,int> neighbor : neighbors)
-            if (find(r.pixels.begin(), r.pixels.end(), neighbor) != r.pixels.end())
-            {
-                border.push_back(pixel);
-                break;
-            }
+/**
+ * Return the perimeter of the two regions if they were to be merged.
+ * It's the sum of their original perimeters minus the length of their intersection.
+ * Complexity of min(n_r1 log n_r2 ; n_r2 log n_r1).
+ *
+ * @param r1 region to merge
+ * @param r2 region to merge
+ * @return perimeter of the merged region
+ */
+int intersectionLength(Region r1, Region r2)
+{
+    // checking which region is smaller
+    Region smallRegion;
+    Region bigRegion;
+    if (r1.pixels.size() < r2.pixels.size())
+    {
+        smallRegion = r1;
+        bigRegion = r2;
     }
-    return border;
+    else
+    {
+        smallRegion = r2;
+        bigRegion = r1;
+    }
+
+    // looping through smallest region to check
+    // intersection pixels with bigger region
+    int length = 0;
+    for (Pixel pixel : smallRegion.pixels) // n
+    {
+        Pixel* neighbors = getNeighbors(pixel);
+        for (int i = 0; i < 4; ++i) // 4n
+            if (bigRegion.pixels.find(neighbors[i]) != bigRegion.pixels.end()) // log(n)
+                length++;
+    }
+    return length;
 }
 
 double perimeter(Mat image, Region r)
@@ -88,7 +116,7 @@ double perimeter(Mat image, Region r)
     double perimeter = 0.;
 
     // border detection
-    vector<pair<int,int>> border = findBorder(r);
+    //vector<Pixel> border = findBorder(r);
 
     return perimeter;
 }
@@ -98,17 +126,46 @@ double energy(Mat image, Region r, double lambda)
     return variance(image, r) + lambda * perimeter(image, r);
 }
 
-Region merge(Region r1, Region r2)
+/**
+ * Merges two regions by adding the pixels of r2 into r1 without duplicating them.
+ *
+ * @param r1 will have r2 merged into
+ * @param r2 will be merged into r1
+ */
+void merge(Region r1, Region r2)
 {
     Region result;
 
+    r1.pixels.merge(r2.pixels);
+    r1.perimeter = r1.perimeter + r2.perimeter - intersectionLength(r1, r2);
 
-
-    return result;
+    r1.adjacentRegions.merge(r2.adjacentRegions);
 }
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
+
+    Region r1;
+    Region r2;
+
+    r1.adjacentRegions.emplace(4);
+    r2.adjacentRegions.emplace(4);
+    r2.adjacentRegions.emplace(4);
+    r2.adjacentRegions.emplace(4);
+    r2.adjacentRegions.emplace(4);
+    r2.adjacentRegions.emplace(5);
+
+    r1.adjacentRegions.merge(r2.adjacentRegions);
+
+    cout << "r1 :" << endl;
+    for (auto a : r1.adjacentRegions)
+        cout << a << " ";
+    cout << endl;
+    cout << "r2 :" << endl;
+    for (auto a : r2.adjacentRegions)
+        cout << a;
+    cout << endl;
+
     return 0;
 }
